92202a1e49421c938af401e972674f10236a97ec
Peter Tsai <yutingtsai0924@gmail.com>
2026-07-25 13:56:40 +0900

    Add greeting feature

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Day4");
+
+        System.out.println("Day5 feature branch practice");
+
     }
 }
